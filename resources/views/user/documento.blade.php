c876c56ac365e70bebdeb391302a5968d9f8824a
Aikeson123 <107086003+Aikeson123@users.noreply.github.com>
2022-06-29 13:21:37 -0600

    plantilla documentacion

--- resources/views/user/documento.blade.php
+++ resources/views/user/documento.blade.php
@@ -5,11 +5,18 @@
 @section('content')
     <div class="contieneDatos">
         <div class="datosAutor">
-            <label class="title">{{$categoria}}</label>
+            <h1 style="font-size:2em; font-weight: bold; text-align: center;">Documentación</h1><br>
         </div>
         <div class="archivos">
             @foreach($archivos as $archivo)
-                <label></label>
+                <label style="font-size:1.5em; font-weight: bold;">*Titulo del archivo*</label>
+                <table>
+                <tr>
+                    <td style="text-align: center; font-size:10em;">
+                        *Contenido*
+                    </td>
+                </tr>
+                </table>
             @endforeach
         </div>
     </div>
